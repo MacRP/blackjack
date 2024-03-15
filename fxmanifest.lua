867f15ee28475cfd3883a979947a6faa842d6d28
Mac <145077500+MacRP@users.noreply.github.com>
2024-03-15 18:36:29 -0400

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Your Name'
+description 'Blackjack Script for FiveM QBCore'
+version '1.0.0'
+
+client_scripts {
+    'client.lua',
+}
+
+server_script 'server.lua'
